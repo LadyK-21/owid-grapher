1591dabd2df01ac5df86e98b55248e66b01db2e5
Marcel Gerber <mgerber59@gmail.com>
2026-05-06 11:58:03 +0200

    enhance(npmrc): write what we're doing

--- devTools/harden-npmrc.sh
+++ devTools/harden-npmrc.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo "Updating npm..."
+
 # Upgrade npm globally (so we have at least 11.10.0, which introduced min-release-age) and create a user npm config with stricter defaults.
 npm install -g npm
 
@@ -9,7 +11,12 @@ if [[ -e "$HOME/.npmrc" ]]; then
     exit 1
 fi
 
+echo "Putting in place a ~/.npmrc file..."
+
 cat >"$HOME/.npmrc" <<'EOF'
+# These settings are intended to avoid supply chain attacks coming in via running npm or npx. They are recommended by OWID.
 min-release-age=1 # days
 ignore-scripts=true
 EOF
+
+echo "Done."
